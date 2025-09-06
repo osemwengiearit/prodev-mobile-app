aa9c612a589ec169e7469e8a3f4bf16a541cec7e
Uyearit <uyearit23@gmail.com>
2025-09-06 23:34:47 +0100

    feat:Complete project initialization process

---
+++ prodev-mobile-app-0x04/constants/index.ts
@@ -0,0 +1,11 @@
+const BACKGROUNDIMAGE = require('../assets/images/hero-icon.png').default;
+const HEROLOGO = require('../assets/images/Logo.png').default;
+const HEROLOGOGREEN = require('../assets/images/logo-green.png').default;
+const GOOGLELOGO = require('../assets/images/google.png').default;
+const FACEBOOKLOGO = require('../assets/images/facebook.png').default;
+
+export {
+    BACKGROUNDIMAGE, FACEBOOKLOGO, GOOGLELOGO, HEROLOGO,
+    HEROLOGOGREEN
+};
+
