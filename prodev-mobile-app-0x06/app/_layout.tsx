45074e490c290ed96b52b06e781a66c98dc49a4b
Uyearit <uyearit23@gmail.com>
2025-09-08 08:23:19 +0100

    refactor(navigation): update Expo Router layouts for NativeWind styling

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
+      <Stack.Screen name="(home)" />
     </Stack>
   );
 }
